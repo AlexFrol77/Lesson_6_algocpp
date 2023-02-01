35371d32e66c213ab0fad97b5ac6ed692ec0831a
Alex Frol <drbo19777@gmail.com>
2023-02-01 21:37:37 +0300

    Init commit

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include <Windows.h>
+
+
+int main(int argc, char** argv) {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	std::string entry;
+	int temp = 0;
+	
+	do {
+		temp = 0;
+		std::cout << "Введите строку: ";
+		std::cin >> entry;
+		for (char i : entry) {
+			temp += static_cast<int>(abs(i));
+		}
+		std::cout << "Наивный хэш строки " << entry << " = " << temp << std::endl;
+	} while (entry != "exit");
+
+	return 0;
+}
